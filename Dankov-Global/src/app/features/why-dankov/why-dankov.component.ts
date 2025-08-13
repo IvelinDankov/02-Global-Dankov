13e90ba49f3f1bcb647a963262db12dfbd35b7f9
Velin Dankov <velin80@web.de>
2025-08-13 18:04:22 +0300

    making readmore content toggle

--- Dankov-Global/src/app/features/why-dankov/why-dankov.component.ts
+++ Dankov-Global/src/app/features/why-dankov/why-dankov.component.ts
@@ -7,4 +7,14 @@ import { Component, CUSTOM_ELEMENTS_SCHEMA } from "@angular/core";
   styleUrl: "./why-dankov.component.scss",
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
-export class WhyDankovComponent {}
+export class WhyDankovComponent {
+  cssClass: boolean = false;
+  cssClassP: boolean = false;
+
+  showText() {
+    this.cssClass = !this.cssClass;
+  }
+  showTextP() {
+    this.cssClassP = !this.cssClassP;
+  }
+}
